810db1ee15ae484de2e21d5c8ec13bdd9406da0f
bilalafzal97 <bilalafzal245256@gmail.com>
2022-08-12 09:03:12 +0500

    Marketplace api methods

---
+++ Runtime/Models/Response/Marketplace/ListingResponse.cs
@@ -0,0 +1,30 @@
+using System;
+using Newtonsoft.Json;
+
+namespace mirrorworld_sdk_unity.Runtime.Models.Response.Marketplace
+{
+    public class ListingResponse
+    {
+        [JsonProperty("id")] public long Id;
+
+        [JsonProperty("type")] public string Type;
+
+        [JsonProperty("wallet_address")] public string WalletAddress;
+
+        [JsonProperty("mint_address")] public string MintAddress;
+
+        [JsonProperty("price")] public decimal Price;
+
+        [JsonProperty("seller_address")] public string SellerAddress;
+
+        [JsonProperty("signature")] public string Signature;
+
+        [JsonProperty("status")] public string Status;
+
+        [JsonProperty("updatedAt")] public DateTime? UpdatedAt;
+
+        [JsonProperty("createdAt")] public DateTime? CreatedAt;
+
+        [JsonProperty("to_wallet_address")] public string ToWalletAddress;
+    }
+}
