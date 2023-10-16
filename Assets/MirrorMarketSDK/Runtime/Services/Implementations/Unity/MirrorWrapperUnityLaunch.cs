3dac71bbecad4772f8c484464481dd738e787cd8
Squall <2573040560@qq.com>
2023-10-16 18:22:40 +0800

    uncomment save function

--- Assets/MirrorMarketSDK/Runtime/Services/Implementations/Unity/MirrorWrapperUnityLaunch.cs
+++ Assets/MirrorMarketSDK/Runtime/Services/Implementations/Unity/MirrorWrapperUnityLaunch.cs
@@ -91,12 +91,6 @@ namespace MirrorworldSDK.Wrapper
             return monoBehaviour;
         }
 
-        private void SetLoginResponse(LoginResponse response)
-        {
-            SaveStringToLocal(localKeyRefreshToken, response.refresh_token);
-            accessToken = response.access_token;
-        }
-
         public void LogFlow(string content)
         {
             if (debugMode)
